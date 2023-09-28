be8b20593a80a3c8bda8e08ab35d5f49634bcdab
MOHAMMAD SUMON <67429465+sumu9897@users.noreply.github.com>
2023-09-28 09:07:06 +0600

    Delete 30 Days of Pandas/1757. Recyclable and Low Fat Products.py

--- 30 Days of Pandas/1757. Recyclable and Low Fat Products.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def find_products(products: pd.DataFrame) -> pd.DataFrame:
-    return products[
-        (products['low_fats'] == 'Y') & \
-        (products['recyclable'] == 'Y')
-    ][['product_id']]
